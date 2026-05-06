87800c8c574dc5f997d3619eec7eecd482f05c32
aleexxmorenno <164919518+aleexxmorenno@users.noreply.github.com>
2026-05-07 00:09:55 +0200

    Add files via upload

--- agent.py
+++ agent.py
@@ -1,16 +1,33 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search
+from google.adk.tools.agent_tool import AgentTool
 
-news_analyst = Agent(
-    name="news_analyst",
+from .sub_agents.book_recommender.agent import book_recommender
+from .sub_agents.news_analyst.agent import news_analyst
+from .sub_agents.crypto_analyst.agent import crypto_analyst
+from .tools.tools import get_current_time
+
+root_agent = Agent(
+    name="manager",
     model="gemini-2.0-flash",
-    description="News analyst agent",
+    description="Manager agent",
     instruction="""
-    You are a helpful assistant that can analyze news articles and provide a summary of the news.
+    You are a manager agent responsible for overseeing and delegating work 
+    to the appropriate specialized agents.
 
-    When asked about news, you should use the google_search tool to search for the news.
+    Always delegate the user's request to the most suitable sub-agent.
+    Use your best judgment to decide which agent can best accomplish the task.
 
-    If the user ask for news using a relative time, you should use the get_current_time tool to get the current time to use in the search query.
+    You can delegate tasks to the following agents:
+    - crypto_analyst
+    - book_recommender
+
+    You also have access to the following tools:
+    - news_analyst
+    - get_current_time
     """,
-    tools=[google_search],
+    sub_agents=[crypto_analyst, book_recommender],
+    tools=[
+        AgentTool(news_analyst),
+        get_current_time,
+    ],
 )
